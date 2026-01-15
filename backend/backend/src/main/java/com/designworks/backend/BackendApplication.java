d5892aefec91d7f38b643219866b52ce6941098c
luis <luissscj678@gmail.com>
2026-01-15 13:28:50 +0100

    Add initial Spring Boot backend project and configure Git line endings

---
+++ backend/backend/src/main/java/com/designworks/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.designworks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
