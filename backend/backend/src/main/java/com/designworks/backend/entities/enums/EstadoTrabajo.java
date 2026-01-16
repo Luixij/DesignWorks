392172e2b5fa547c77a76e0408cd9da0f3f376f2
luis <luissscj678@gmail.com>
2026-01-16 12:43:47 +0100

    Add EstadoTrabajo and Prioridad enums

---
+++ backend/backend/src/main/java/com/designworks/backend/entities/enums/EstadoTrabajo.java
@@ -0,0 +1,9 @@
+package com.designworks.backend.entities.enums;
+
+public enum EstadoTrabajo {
+    CREADO,
+    EN_PROGRESO,
+    EN_REVISION,
+    ENTREGADO,
+    CANCELADO
+}
---
+++ backend/backend/src/main/java/com/designworks/backend/entities/enums/Prioridad.java
@@ -0,0 +1,8 @@
+package com.designworks.backend.entities.enums;
+
+public enum Prioridad {
+    BAJA,
+    MEDIA,
+    ALTA,
+    URGENTE
+}
