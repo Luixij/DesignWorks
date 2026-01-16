d4e016e2f6dd855e61d49053b3bf2c35f0d96fe7
luis <luissscj678@gmail.com>
2026-01-16 09:08:27 +0100

    Create UsuarioRepository with findByEmail and existsByEmail methods

---
+++ backend/backend/src/main/java/com/designworks/backend/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.designworks.backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.designworks.backend.entities.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
